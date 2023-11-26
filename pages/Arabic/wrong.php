1e92e2e7931317f4618278bd296a665eff78e800
MohammedRaouf99 <103843734+MohammedRaouf99@users.noreply.github.com>
2023-11-26 13:01:30 +0000

    Add files via upload

---
+++ pages/Arabic/wrong.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <style>
+        div {
+            
+		background-color: white;
+        width: auto;
+		color : red ;
+		height: 80px;
+		border: 2px solid red;
+		border-radius: 5px;
+		padding-top: 20px;
+		align-content: center;
+		text-align: center;
+		font-size: 18px;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		direction: rtl;
+	}
+    
+	img {
+		max-width: 100%;
+		height: 86px;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+    
+</style>
+<body>
+	<img src="wrong.png">
+	<div>
+		<b>عفواً كلمة المرور غير صحيحة الرجاء المحاوله مره اخرى</b>
+
+	</div>
+</body>
+</html>
+<?php
+header("Refresh: 2 ; url= index.html")
+?>
---
+++ pages/English/true.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <style>
+        div {
+            
+		background-color: white;
+        width: auto;
+		color : rgb(5, 236, 51);
+		height: 80px;
+		border: 2px solid green;
+		border-radius: 5px;
+		padding-top: 20px;
+		align-content: center;
+		text-align: center;
+		font-size: 22px;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		direction: rtl;
+	}
+    
+	img {
+		max-width: 100%;
+		height: 86px;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	
+    
+</style>
+<body>
+	<img src="true.png">
+	<div>
+		<b>The password is correct, the call will be restored now</b>
+
+	</div>
+</body>
+</html>
+<?php
+
+?>
